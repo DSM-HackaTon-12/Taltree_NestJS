4d41559fc9891c9bcbbb62426dbb01df5a5381d3
soyeonkim0227 <soyeonkim0227@naver.com>
2024-07-17 18:49:50 +0900

    CORS 문제 해결

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { HttpExceptionFilter } from './filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
